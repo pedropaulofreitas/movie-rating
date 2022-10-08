43be4a4972f74e4142e346ebb6754b33109ca4c8
explorer <pedro.freitas@shipay.com.br>
2022-10-08 10:42:48 -0300

    feature(movie-list): movies interface and table with mocked values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MovieListComponent } from './pages/movie-list/movie-list.component';
+import { MatTableModule } from '@angular/material/table'  
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MovieListComponent } from './pages/movie-list/movie-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -1,5 +1,116 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+export interface Movie {
+  year: string,
+  votes: string,
+  title: string,
+  runtime: string,
+  revenue: string,
+  director: string,
+  description: string,
+  rating: string,
+  rank: string,
+  metascore: string,
+  genre: string[],
+  actors: string[]
+}
+
+const MOVIE_DATA: Movie[] = [
+  {
+    "year": "2014",
+    "votes": "757074",
+    "title": "Guardians of the Galaxy",
+    "runtime": "121",
+    "revenue": "333.13",
+    "rating": "8.1",
+    "rank": "1",
+    "metascore": "76",
+    "genre": [
+      "Action",
+      "Adventure",
+      "Sci-Fi"
+    ],
+    "director": "James Gunn",
+    "description": "A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.",
+    "actors": [
+      "Chris Pratt",
+      "Vin Diesel",
+      "Bradley Cooper",
+      "Zoe Saldana"
+    ]
+  },
+  {
+    "year": "2012",
+    "votes": "485820",
+    "title": "Prometheus",
+    "runtime": "124",
+    "revenue": "126.46",
+    "rating": "7",
+    "rank": "2",
+    "metascore": "65",
+    "genre": [
+      "Adventure",
+      "Mystery",
+      "Sci-Fi"
+    ],
+    "director": "Ridley Scott",
+    "description": "Following clues to the origin of mankind, a team finds a structure on a distant moon, but they soon realize they are not alone.",
+    "actors": [
+      "Noomi Rapace",
+      "Logan Marshall-Green",
+      "Michael Fassbender",
+      "Charlize Theron"
+    ]
+  },
+  {
+    "year": "2016",
+    "votes": "157606",
+    "title": "Split",
+    "runtime": "117",
+    "revenue": "138.12",
+    "rating": "7.3",
+    "rank": "3",
+    "metascore": "62",
+    "genre": [
+      "Horror",
+      "Thriller"
+    ],
+    "director": "M. Night Shyamalan",
+    "description": "Three girls are kidnapped by a man with a diagnosed 23 distinct personalities. They must try to escape before the apparent emergence of a frightful new 24th.",
+    "actors": [
+      "James McAvoy",
+      "Anya Taylor-Joy",
+      "Haley Lu Richardson",
+      "Jessica Sula"
+    ]
+  },
+  {
+    "year": "2016",
+    "votes": "60545",
+    "title": "Sing",
+    "runtime": "108",
+    "revenue": "270.32",
+    "rating": "7.2",
+    "rank": "4",
+    "metascore": "59",
+    "genre": [
+      "Animation",
+      "Comedy",
+      "Family"
+    ],
+    "director": "Christophe Lourdelet",
+    "description": "In a city of humanoid animals, a hustling theater impresario's attempt to save his theater with a singing competition becomes grander than he anticipates even as its finalists' find that their lives will never be the same.",
+    "actors": [
+      "Matthew McConaughey",
+      "Reese Witherspoon",
+      "Seth MacFarlane",
+      "Scarlett Johansson"
+    ]
+  }
+];
+
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
@@ -7,9 +118,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['title', 'year', 'runtime', 'revenue', 'rating', 'genre'];
+  dataSource = MOVIE_DATA;
+  
 
+  constructor() { }
+  
   ngOnInit(): void {
   }
 
 }
+
+
