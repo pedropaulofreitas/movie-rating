69d7e82e3b1fae1e1f370d07bd03f40b1ac34292
explorer <pedro.freitas@shipay.com.br>
2022-10-08 10:13:45 -0300

    feature(movie-list): add movie list to router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '<div id="root"><router-outlet></router-outlet></div>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
